1acc38574a7812e04115b9729e47d242c66b24c9
Mateusz <mateusz.makowski.102@gmail.com>
2025-04-01 15:13:33 +0200

    Added events handler for voiceChannelUpdate

--- bot/events.py
+++ bot/events.py
@@ -1,4 +1,6 @@
 from config.settings import CHANNELS_WITHOUT_TRACKING
+from database.models import User, VoiceActivity
+from datetime import datetime
 
 
 def client_event_declaration(bot):
@@ -13,13 +15,25 @@ def client_event_declaration(bot):
 
     @bot.event
     async def on_voice_state_update(member, before, after):
-        print(f'member: {member}')
-        print(f'before: {before.channel}')
-        print(f'after: {after}')
-        if (after.channel.id in CHANNELS_WITHOUT_TRACKING):
-            print(f'User {member} moved to silent-zone')
-            return
-        if (bool(not before.channel and after.channel)):
-            print(f'{member} joined {after.channel}')
-        else:
-            print(f'{member} left {before.channel}')
+        user: User = User.objects(discord_id=member.id).first()
+        print(before.channel, after.channel)
+        if (bool(user)):
+            now = datetime.now()
+            if (bool(not before.channel and after.channel)):
+                voice_activity = VoiceActivity(
+                    user_id=user.discord_id,
+                    username=user.name,
+                    channel_id=after.channel.id,
+                    channel_name=after.channel.name,
+                    joined_at=now.strftime('%Y-%m-%d %H:%M:%S'),
+                )
+                voice_activity.save()
+            else:
+                current_voice_activity = VoiceActivity.objects(
+                    user_id=user.discord_id, left_at=None).first()
+                joined_converted = datetime.strptime(
+                    current_voice_activity.joined_at, '%Y-%m-%d %H:%M:%S')
+                delta = (now - joined_converted).total_seconds()/60
+                current_voice_activity.update(
+                    left_at=now.strftime('%Y-%m-%d %H:%M:%S'),
+                    session_duration=round(delta, 2))
--- database/models.py
+++ database/models.py
@@ -1,12 +1,24 @@
 from mongoengine import *
-import datetime
+from datetime import datetime, timezone
 
 
 class User(Document):
     name = StringField(required=True)
-    updated_at = DateTimeField(default=datetime.datetime.utcnow)
+    updated_at = DateTimeField(default=datetime.now(timezone.utc))
     discord_name = StringField(required=True)
     discord_avatar = URLField(required=True)
     is_online = BooleanField(default=False)
     discord_id = IntField(required=True)
     meta = {'collection': 'bot-users'}
+
+
+class VoiceActivity(Document):
+    user_id = IntField(required=True)
+    username = StringField(required=True)
+    channel_id = IntField(required=True)
+    channel_name = StringField(required=True)
+    joined_at = StringField(required=True)
+    left_at = StringField(default=None)
+    shared_users = ListField(anything=True, default=[])
+    session_duration = FloatField(default=0)
+    meta = {'collection': 'voice-activity'}
