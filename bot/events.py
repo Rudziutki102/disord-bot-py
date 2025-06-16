f92735bc1db7728d387cdded19699adf18ff15ee
Mateusz <mateusz.makowski.102@gmail.com>
2025-06-16 19:47:55 +0200

    'test'

--- bot/events.py
+++ bot/events.py
@@ -1,6 +1,9 @@
 from config.settings import CHANNELS_WITHOUT_TRACKING
 from database.models import User, VoiceActivity
 from datetime import datetime
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def client_event_declaration(bot):
@@ -15,22 +18,28 @@ def client_event_declaration(bot):
 
     @bot.event
     async def on_voice_state_update(member, before, after):
-        user: User = User.objects(discord_id=member.id).first()
-        if (bool(user)):
+        user: User = User.objects(discord_id=str(member.id)).first()
+        if (user):
             now = datetime.now()
-            if (bool(not before.channel and after.channel)):
-                voice_activity = VoiceActivity(
-                    user_id=user.discord_id,
-                    username=user.name,
-                    channel_id=after.channel.id,
-                    channel_name=after.channel.name,
-                    joined_at=now,
-                )
-                voice_activity.save()
+            if (not before.channel and after.channel):
+                try:
+                    voice_activity = VoiceActivity(
+                        user_id=str(user.discord_id),
+                        username=user.name,
+                        channel_id=str(after.channel.id),
+                        channel_name=after.channel.name,
+                        joined_at=now,
+                    )
+                    voice_activity.save()
+                except Exception as e:
+                    logger.exception('błąd', e)
             else:
                 current_voice_activity = VoiceActivity.objects(
                     user_id=user.discord_id, left_at=None).first()
                 delta = (now - current_voice_activity.joined_at).total_seconds()/60
-                current_voice_activity.update(
-                    left_at=now,
-                    session_duration=round(delta, 2))
+                try:
+                    current_voice_activity.update(
+                        left_at=now,
+                        session_duration=round(delta, 2))
+                except Exception as e:
+                    logger.exception('błąd', e)
--- database/models.py
+++ database/models.py
@@ -8,14 +8,14 @@ class User(Document):
     discord_name = StringField(required=True)
     discord_avatar = URLField(required=True)
     is_online = BooleanField(default=False)
-    discord_id = IntField(required=True)
+    discord_id = StringField(required=True)
     meta = {'collection': 'bot-users'}
 
 
 class VoiceActivity(Document):
-    user_id = IntField(required=True)
+    user_id = StringField(required=True)
     username = StringField(required=True)
-    channel_id = IntField(required=True)
+    channel_id = StringField(required=True)
     channel_name = StringField(required=True)
     joined_at = DateTimeField(required=True)
     left_at = DateTimeField(default=None)
