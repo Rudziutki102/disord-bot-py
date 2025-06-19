9c1d396f2da1d4b460bb589b05a8a5a37fa6d613
Mateusz <mateusz.makowski.102@gmail.com>
2025-06-19 19:57:01 +0200

    Added behavior for changing voice state

--- bot/events.py
+++ bot/events.py
@@ -17,52 +17,55 @@ def client_event_declaration(bot):
             print(e)
 
     @bot.event
+    # myślę że rzeczywiście dobrym pomysłem jest przeniesienie tych rzeczy do osobnych funkcji dla czytelności dodatkowo dodamy jeszcze potem eventy taie jak streaming czy mute
     async def on_voice_state_update(member, before, after):
         user: User = User.objects(discord_id=str(member.id)).first()
+        if not user:
+            return
         if (user):
             now = datetime.now()
-            if (not before.channel and after.channel):
-                try:
-                    voice_activity = VoiceActivity(
-                        user_id=str(user.discord_id),
-                        username=user.name,
-                        channel_id=str(after.channel.id),
-                        channel_name=after.channel.name,
-                        joined_at=now,
+            current_voice_activity = VoiceActivity.objects(
+                user_id=user.discord_id, left_at=None
+            ).first()
+            if current_voice_activity:
+                delta = (
+                    now - current_voice_activity.joined_at).total_seconds()/60
+            try:
+                if (not before.channel and after.channel):
+                    # first join
+                    if str(after.channel.id) not in CHANNELS_WITHOUT_TRACKING:
+                        voice_activity = VoiceActivity(
+                            user_id=str(user.discord_id),
+                            username=user.name,
+                            channel_id=str(after.channel.id),
+                            channel_name=after.channel.name,
+                            joined_at=now,
+                        )
+                        voice_activity.save()
+                elif before.channel and not after.channel:
+                    # leaves permanently
+                    VoiceActivity.objects(user_id=user.discord_id, left_at=None).update_one(
+                        set__left_at=now,
+                        set__session_duration=round(delta, 2)
                     )
-                    voice_activity.save()
-                except Exception as e:
-                    logger.exception('błąd', e)
-            elif before.channel and not after.channel:
-                current_voice_activity = VoiceActivity.objects(
-                    user_id=user.discord_id, left_at=None).first()
-                delta = (now - current_voice_activity.joined_at).total_seconds()/60
-                try:
-                    current_voice_activity.update(
-                        left_at=now,
-                        session_duration=round(delta, 2))
-                except Exception as e:
-                    logger.exception('błąd', e)
-            else:
-                current_voice_activity = VoiceActivity.objects(
-                    user_id=user.discord_id, left_at=None).first()
-                delta = (now - current_voice_activity.joined_at).total_seconds()/60
-                if str(after.channel.id) in CHANNELS_WITHOUT_TRACKING:
-                    try:
-                        current_voice_activity.update(
-                            left_at=now,
-                            session_duration=round(delta, 2))
-                    except Exception as e:
-                        logger.exception('błąd', e)
                 else:
-                    current_voice_activity.update(
-                        left_at=now,
-                        session_duration=round(delta, 2))
-                    voice_activity = VoiceActivity(
-                        user_id=str(user.discord_id),
-                        username=user.name,
-                        channel_id=str(after.channel.id),
-                        channel_name=after.channel.name,
-                        joined_at=now,
-                    )
-                    voice_activity.save()
+                    # switching between channels
+                    if str(after.channel.id) in CHANNELS_WITHOUT_TRACKING:
+                        VoiceActivity.objects(user_id=user.discord_id, left_at=None).update_one(
+                            set__left_at=now,
+                            set__session_duration=round(delta, 2))
+                    else:
+                        if current_voice_activity:
+                            VoiceActivity.objects(user_id=user.discord_id, left_at=None).update_one(
+                                set__left_at=now,
+                                set__session_duration=round(delta, 2))
+                        voice_activity = VoiceActivity(
+                            user_id=str(user.discord_id),
+                            username=user.name,
+                            channel_id=str(after.channel.id),
+                            channel_name=after.channel.name,
+                            joined_at=now,
+                        )
+                        voice_activity.save()
+            except Exception as e:
+                logger.exception(f'Błąd przy zapisie : {e}')
